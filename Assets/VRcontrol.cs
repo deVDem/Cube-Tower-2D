042f7a9285e558c47b7b5a2325bda546075fa851
Roman Medvedev <roman.medvedev@devdem.ru>
2021-08-06 01:31:07 +0300

    VR Ready

--- Assets/VRcontrol.cs
+++ Assets/VRcontrol.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class VRcontrol : MonoBehaviour
 {
+    bool game = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,10 @@ public class VRcontrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!game && SceneManager.GetActiveScene().name == "Game")
+        {
+            transform.position = new Vector3(-20.96f, 28f, -93.81f);
+            game = true;
+        }
     }
 }
